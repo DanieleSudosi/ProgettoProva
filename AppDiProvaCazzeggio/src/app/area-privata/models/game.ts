ccc1156d5cfaa17495f44c52ada5adccc97fda6f
Simone Sudosi <simonesudosi@yahoo.it>
2022-04-24 17:37:57 +0200

    cat

--- AppDiProvaCazzeggio/src/app/area-privata/models/game.ts
+++ AppDiProvaCazzeggio/src/app/area-privata/models/game.ts
@@ -2,6 +2,9 @@ export class Game{
     title: string;
     house: string;
     genre: string;
+    publisher: string;
+    release: string;
+    price: number;
     description: string;
     id?: number;
 }
---
+++ AppDiProvaCazzeggio/src/app/area-privata/models/manga.ts
@@ -0,0 +1,10 @@
+export class manga{
+    title: string;
+    mangaka: string;
+    genre: string;
+    release: string;
+    status: string;
+    price: number;
+    
+
+}
